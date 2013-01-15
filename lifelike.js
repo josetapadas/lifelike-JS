6906318f2af98ca4ba26efe42d878e7446705403
José Tapadas Alves <jose.tapadas@gmail.com>
2013-01-15 21:31:03 +0000

    We now have a World object with a grid made of points and elements, based on a string map.

--- lifelike.js
+++ lifelike.js
@@ -1,9 +1,29 @@
 // -------- general functions
+var walls = {}
+
+walls.character = "#";
+
 function forEachIn(object, action) {
     for(var property in object) {
 	if(Object.prototype.hasOwnProperty.call(object, property))
 	    action(property, object[property]);
     }
+}
+
+function elementFromChar(c) {
+    if(c == " ")
+	return undefined;
+    else if(c == "#")
+	return walls;
+    else if(c == "o")
+	return new Alentejano();
+}
+
+function charFromElement(element) {
+    if(element == undefined)
+	return " ";
+    else
+	return element.character;
 }
 
 // -------- the Dictionary object
@@ -54,11 +74,11 @@ function Grid(width, height) {
 }
 
 Grid.prototype.valueAt = function(position) {
-    return this.cells[position.y*this.height + position.x];
+    return this.cells[position.y*this.width + position.x];
 }
 
 Grid.prototype.setValueAt = function(position, value) {
-    this.cells[position.y*this.height + position.x] = value;
+    this.cells[position.y*this.width + position.x] = value;
 }
 
 Grid.prototype.isInside = function(position) {
@@ -76,10 +96,80 @@ Grid.prototype.moveValue = function(from, to) {
 }
 
 Grid.prototype.each = function(action) {
-    for(var i=0; i < this.width; i++) {
-	for(var j=0; j < this.height; j++) {
-	    var point = new Point(i, j);
+    for(var i=0; i < this.height; i++) {
+	for(var j=0; j < this.width; j++) {
+	    var point = new Point(j, i);
 	    action(point, this.valueAt(point));
 	}
     }
 }
+
+// -------- the Scenario data
+
+var cave =
+  ["############################",
+   "#                  o       #",
+   "#                          #",
+   "#          #####           #",
+   "#          #   #           #",
+   "#          #####           #",
+   "#                          #",
+   "#    #                     #",
+   "#    #       o             #",
+   "# o  #         o      ######",
+   "#    #                     #",
+   "############################"];
+
+var directions = new Dictionary({
+    "n" : new Point(0, 1),
+    "ne": new Point(1, 1),
+    "nw": new Point(-1, 1),
+    "s": new Point(0, -1),
+    "se": new Point(1, -1),
+    "sw": new Point(-1, -1),
+    "e": new Point(1, 0),
+    "w": new Point(-1, 0)
+});
+
+// -------- Alentejano: only walks south
+function Alentejano() {};
+
+Alentejano.prototype.action = function() {
+    return {type: "move", direction: "s"};
+}
+
+Alentejano.prototype.character = "o";
+
+// -------- the World object
+
+function World(map) {
+    var grid = new Grid(map[0].length, map.length);
+
+    for(var i=0; i < map.length; i++) {
+	var line = map[i];
+
+	for(var j=0; j < line.length; j++) {
+	    grid.setValueAt(new Point(j, i), elementFromChar(line.charAt(j)));
+	}
+    }
+
+    this.grid = grid;
+}
+
+World.prototype.toString = function() {
+    var chars = [];
+    var EOL = this.grid.width - 1;
+
+    this.grid.each(function (point, value) {
+	chars.push(charFromElement(value));
+
+	if(point.x == EOL)
+	    chars.push("\n");
+    });
+
+    return chars.join("");
+}
+
+var mundo = new World(cave);
+console.log(mundo.toString());
+
