8da045c2947a71e8a5b7172bd86e5eb5c55f8345
José Alves <jose.alves@custojusto.pt>
2013-01-15 16:53:31 +0000

    Added the Dictionary object, with some helper methods.

--- lifelike.js
+++ lifelike.js
@@ -1,3 +1,34 @@
+// -------- general functions
+function forEachIn(object, action) {
+    for(var property in object) {
+	if(Object.prototype.hasOwnProperty.call(object, property))
+	    action(property, object[property]);
+    }
+}
+
+// -------- the Dictionary object
+function Dictionary(values) {
+    this.values = values || {};
+}
+
+Dictionary.prototype.store = function(name, value) {
+    this.values[name] = value;
+} 
+
+Dictionary.prototype.lookup = function(name) {
+    return this.values[name];
+} 
+
+Dictionary.prototype.contains = function(name) {
+    return Object.prototype.hasOwnProperty.call(this.values, name) &&
+	Object.prototype.propertyIsEnumerable.call(this.values, name);
+} 
+
+Dictionary.prototype.each = function(action) {
+    forEachIn(this.values, action);
+} 
+
+
 // -------- the Point object
 function Point(x, y) {
     this.x = x;
@@ -15,3 +46,4 @@ Point.prototype.isEqual = function(point) {
 Point.prototype.toString = function() {
     return "{ x: " + this.x + ", y: " + this.y + " }";
 }
+
